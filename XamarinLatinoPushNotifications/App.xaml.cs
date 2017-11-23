eaddd81ad7300e50afd1d50e0ee82816871d9dc9
Carlos Campos <ccampos@xinobistudio.com>
2017-11-23 16:24:07 -0600

    Added notification handling and process to open up the NotificationPage

--- XamarinLatinoPushNotifications/App.xaml.cs
+++ XamarinLatinoPushNotifications/App.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using Xamarin.Forms;
 using Com.OneSignal;
+using XamarinLatinoPushNotifications.Pages;
 
 namespace XamarinLatinoPushNotifications
 {
     public partial class App : Application
     {
+        public static string MessageFromNotification = "";
         public App()
         {
             InitializeComponent();
@@ -13,6 +15,21 @@ namespace XamarinLatinoPushNotifications
 
             //Set OneSignal
             OneSignal.Current.StartInit("37064466-c9ad-4b6e-9e3c-6df406f13777").HandleNotificationOpened(OnHandleNotificationOpened).EndInit();
+
+            MainPage.Appearing += (sender, e) => 
+            {
+                if(!string.IsNullOrEmpty(MessageFromNotification))
+                {
+                    var notificationPage = new NotificationPage
+                    {
+                        BindingContext = MessageFromNotification,
+                        Title = "Notificacion de OneSignal"
+                    };
+                    var modalPage = new NavigationPage(notificationPage);
+                    MainPage.Navigation.PushModalAsync(modalPage);
+                    MessageFromNotification = "";
+                }
+            };
         }
 
         private void OnHandleNotificationOpened(Com.OneSignal.Abstractions.OSNotificationOpenedResult result)
@@ -20,6 +37,7 @@ namespace XamarinLatinoPushNotifications
             if (result.notification.payload.additionalData.ContainsKey("additional_message"))
             {
                 // Si el payload posee la key additional_message, ejecutar esta seccion de codigo
+                MessageFromNotification = result.notification.payload.additionalData["additional_message"].ToString();
             }
         }
 
