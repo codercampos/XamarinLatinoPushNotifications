cb5b8963db55523a4ab6002fe67492a126a63235
Carlos Campos <ccampos@xinobistudio.com>
2017-11-23 16:01:16 -0600

    Added code to get the Payload information

--- XamarinLatinoPushNotifications/App.xaml.cs
+++ XamarinLatinoPushNotifications/App.xaml.cs
@@ -11,13 +11,16 @@ namespace XamarinLatinoPushNotifications
 
             MainPage = new XamarinLatinoPushNotificationsPage();
 
-            //Setting OneSignal
+            //Set OneSignal
             OneSignal.Current.StartInit("37064466-c9ad-4b6e-9e3c-6df406f13777").HandleNotificationOpened(OnHandleNotificationOpened).EndInit();
         }
 
         private void OnHandleNotificationOpened(Com.OneSignal.Abstractions.OSNotificationOpenedResult result)
         {
-            
+            if (result.notification.payload.additionalData.ContainsKey("additional_message"))
+            {
+                // Si el payload posee la key additional_message, ejecutar esta seccion de codigo
+            }
         }
 
         protected override void OnStart()
